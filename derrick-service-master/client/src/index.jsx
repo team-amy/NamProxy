39c60cb2196303a3b971e8c1194b05b848fdf63f
NamVo5009 <envytheroy@gmail.com>
2019-04-13 18:40:59 -0400

    updated html page, add readme

--- derrick-service-master/client/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SupportSection from './components/SupportSection.jsx';
-
-window.SupportSection= SupportSection;
-
-ReactDOM.render(<SupportSection />, document.getElementById('app'));
--- derrick-service-master/client/src/tests/SupportSection.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SupportSection from '../components/SupportSection.jsx';
-import {shallow, mount} from 'enzyme';
-
-test('<SupportSection/>', () => {
-  let wrapper = shallow(<SupportSection/>);
-  expect(wrapper.exists()).toBe(true);
-});
